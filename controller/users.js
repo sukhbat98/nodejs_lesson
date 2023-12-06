df75833b044b8d3c679d5224f1bca48dbf0c138f
sukhbat98 <svhbat98@gmail.com>
2023-12-06 13:34:54 +0800

    login хийх endppoint-ийг бичлээ

--- controller/users.js
+++ controller/users.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler')
 const path = require("path");
 
+
 // models
 const User = require("../models/User");
 
@@ -14,12 +15,41 @@ const paginate = require("../utils/paginate");
 exports.register = asyncHandler(async (req, res, next) => {
 
   const user = await User.create(req.body)
-  const jwt = user.getJsonWebToken();
+  const token = user.getJsonWebToken();
 
   res.status(200).json({
     success: true,
-    jwt,
+    token,
     data: user,
+  })
+
+});
+
+
+/** User login */
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    throw new MyError("Имэйл болон нууц үгээ дамжуулна уу", 400);
+  }
+
+  const user = await User.findOne({
+    email
+  }).select('+password')
+
+  if (!user) {
+    throw new MyError("Имэйл болон нууц үг буруу байна", 401);
+  }
+
+  const isMatch = await user.checkPassword(password);
+  if (!isMatch) {
+    throw new MyError("Имэйл болон нууц үг буруу байна", 401);
+  }
+
+  res.status(200).json({
+    success: true,
+    token: user.getJsonWebToken(),
   })
 
 });
